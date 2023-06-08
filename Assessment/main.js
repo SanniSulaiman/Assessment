bbab6ca066aaf41685957eda07c2a6ecc588338d
SanniSulaiman <128050622+SanniSulaiman@users.noreply.github.com>
2023-06-08 11:02:31 +0100

    Update main.js

--- Assessment/main.js
+++ Assessment/main.js
@@ -1,18 +1,45 @@
 
 
-const apikey = "https://fakestoreapi.com/products";
+//const apikey = "https://fakestoreapi.com/products";
                 
-fetch('https://fakestoreapi.com/products')
-.then(response =>{
-    return response.json();
-}).then(data =>{
-    const cardAssessment =document.querySelector('Assessment')
-    data.forEach(product => {
-        const productElement = cardAssessment(product);
-        cardAssessment.appendChild(productElement);
+//fetch('https://fakestoreapi.com/products')
+//.then(response =>{
+   // return response.json();
+//}).then(data =>{
+   // const cardAssessment =document.querySelector('Assessment')
+    //data.forEach(product => {
+       // const productElement = cardAssessment(product);
+        //cardAssessment.appendChild(productElement);
     })
-    console.log(data);
-})
+   // console.log(data);
+//})
 
 
+const body = document.body;
+const container = document.createElement("div");
 
+const fetchProduct = async() => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
+  
+  data.map(element => {
+    const card = document.createElement("div");
+    const img = document.createElement("img");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+
+    card.classList.add("card");
+
+    img.setAttribute("src", element.image);
+    h3.textContent = element.title;
+    card.append(img, h3, p);
+    container.append(card);
+  });
+
+
+}
+
+container.classList.add("container")
+
+fetchProduct()
+body.append(container)
